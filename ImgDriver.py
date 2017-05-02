b1b9ebf6d02fa7a3183cc50b9362cf884fe3ce70
brianna-lehman <brilehman29@gmail.com>
2017-05-02 18:17:15 -0400

    outlining

--- ImgDriver.py
+++ ImgDriver.py
@@ -1,12 +1,24 @@
 import Image, ImageChops
-import sys
+import os, sys
 
 def main():
 	path = sys.argv[1]
 	counter = 0
+	images = os.listdir(path)
 
-	for filename in os.listdir(path):
-		imgA = Image.open(filename)
+	for filenameA in images:
+		imgA = Image.open(filenameA)
+
+		for filenameB in images[1:]:
+			imgB = Image.open(filenameB)
+
+			diff = ImageChops.difference(imgA, imgB)
+
+			if diff == 0:
+				os.remove(path+imgB)
+				counter++
+
+	print(counter+" files deleted\n")
 
 # open a path from the command line
 # for every jpeg image in the folder
